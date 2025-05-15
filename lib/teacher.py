c638dc09a3773d45f4927392ad71403dae4f75b3
Youngpuffy <levimuturi2@gmail.com>
2025-05-15 14:33:59 +0300

    inheritance

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,12 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name, subject= None):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+             "OOP", "Inheritance", "Encapsulation", "Polymorphism", "Recursion"
+        ]
 
     def teach(self):
+        return random.choice(self.knowledge)
         pass
